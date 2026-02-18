f10bd185eb6802a21f7d973a1a7e075f1df18cd7
School-account-10 <160393316+School-account-10@users.noreply.github.com>
2026-02-18 18:34:46 +0800

    f

--- expo_Software_and_Site/app_desktop/comprehensure/DASHBOARD/MainDashboard.xaml.cs
+++ expo_Software_and_Site/app_desktop/comprehensure/DASHBOARD/MainDashboard.xaml.cs
@@ -10,19 +10,23 @@ public partial class MainDashboard : ContentPage
 	private async void ProgressButton_Clicked(object sender, EventArgs e)
 	{
 		await DisplayAlert("progress button", "Hi", "hewo btw ok button");
-	}
+        await Navigation.PushAsync(new ProgressDashboard());
+    }
 	private async void ModulesButton_Clicked(object sender, EventArgs e)
 	{
-        await DisplayAlert("modules button", "Hi", "hewo btw ok button");
+		await DisplayAlert("modules button", "Hi", "hewo btw ok button");
+        await Navigation.PushAsync(new ModulesDaskboard());
     }
 
 	private async void ProfileButton_Clicked(object sender, EventArgs e)
 	{
-        await DisplayAlert("profile button", "Hi", "hewo btw ok button");
+		await DisplayAlert("profile button", "Hi", "hewo btw ok button");
+        await Navigation.PushAsync(new ProfileDashboard());
     }
-    private async void AboutUsButton_Clicked(object sender, EventArgs e)
-    {
-        await DisplayAlert("About US button", "Hi", "hewo btw ok button");
+	private async void AboutUsButton_Clicked(object sender, EventArgs e)
+	{
+		await DisplayAlert("About US button", "Hi", "hewo btw ok button");
+        await Navigation.PushAsync(new AllAboutUS_Dashboard());
     }
 
 }
